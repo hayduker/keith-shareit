6ab11bb0dfe042e61d8df88760a3f8d814549f8f
Derek Smith <github@sandy.simplelogin.com>
2026-06-19 11:34:40 -0600

    Set up basic cli and blob sending

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,37 @@
 use anyhow::Result;
-use iroh::{Endpoint, SecretKey, endpoint::presets};
+use clap::{Parser, Subcommand};
+use iroh::{Endpoint, SecretKey, endpoint::presets, protocol::Router};
+use iroh_blobs::{BlobsProtocol, store::mem::MemStore, ticket::BlobTicket};
+use std::path::PathBuf;
+
+/// A simple file transfer utility
+#[derive(Parser)]
+#[command(name = "d2j", about = "Send and receive files peer-to-peer", long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Send a file and generate a ticket
+    Send {
+        /// The path to the file you want to send
+        filename: PathBuf,
+    },
+    /// Receive a file using a ticket
+    Receive {
+        /// The connection ticket provided by the sender
+        ticket: String,
+        /// The path where the received file should be saved
+        filename: PathBuf,
+    },
+}
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    let cli = Cli::parse();
+
     let secret_key = find_or_create_secret_key("endpoint.key")?;
 
     let endpoint = Endpoint::builder(presets::N0)
@@ -10,11 +39,26 @@ async fn main() -> Result<()> {
         .bind()
         .await?;
 
-    endpoint.online().await;
-
     let endpoint_id = endpoint.id();
-
     println!("My endpoint ID = {}", endpoint_id);
+
+    let store = MemStore::new();
+    let blobs = BlobsProtocol::new(&store, None);
+
+    match cli.command {
+        Commands::Send { filename } => {
+            println!("Preparing to send {}", filename.display());
+            let abs_path = std::path::absolute(&filename)?;
+
+            println!("Hashing file...");
+            let tag = store.blobs().add_path(abs_path).await?;
+
+            let ticket = BlobTicket::new(endpoint_id.into(), tag.hash, tag.format);
+            println!("File hashed. Fetch the file by running:");
+            println!("cargo run -- receive {ticket} {}", filename.display());
+        }
+        Commands::Receive { ticket, filename } => {}
+    }
 
     Ok(())
 }
