c9c7e7283966f118bb60dd6315428fc37756784b
Derek Smith <github@sandy.simplelogin.com>
2026-06-19 10:44:02 -0600

    Set up endpoint with persistent id

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use iroh::{Endpoint, SecretKey, endpoint::presets};
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let secret_key = find_or_create_secret_key("endpoint.key")?;
+
+    let endpoint = Endpoint::builder(presets::N0)
+        .secret_key(secret_key)
+        .bind()
+        .await?;
+
+    endpoint.online().await;
+
+    let endpoint_id = endpoint.id();
+
+    println!("My endpoint ID = {}", endpoint_id);
+
+    Ok(())
+}
+
+fn find_or_create_secret_key(filename: &str) -> Result<SecretKey> {
+    match std::fs::read(filename) {
+        Ok(bytes) => Ok(SecretKey::from_bytes(&bytes.as_slice().try_into()?)),
+        Err(_) => {
+            let key = SecretKey::generate();
+            std::fs::write(filename, key.to_bytes())?;
+            Ok(key)
+        }
+    }
 }
